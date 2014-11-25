fce077574e2927309109b96c8f34108d95a52865
bivdub <bivdub@gmail.com>
2014-11-25 15:34:21 -0800

    got the for loop alternatives working

--- reddit.js
+++ reddit.js
@@ -1,5 +1,29 @@
 var reddit = require("./data.json")
 
-// Write your solutions below
+var array = reddit["data"].children
+// console.log(reddit["data"]);
+console.log(array.length)
 
-console.log(reddit["data"]);
+array.forEach(function(element) {
+	console.log(element.data.title);
+})
+
+var linkArray = array.map(function(e) {
+	return (e.data.url);
+})
+
+console.log(linkArray);
+
+var selfArray = array.filter(function(e) {
+	return e.data.selftext !== "";
+}).map(function(e) {
+	return (e.data.url);
+})
+
+console.log(selfArray, selfArray.length);
+
+var score = array.reduce(function(x, y) {
+	return (x + y.data.score);
+}, 0)
+
+console.log(score);
